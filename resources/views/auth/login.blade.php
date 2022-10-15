7b6ee7458c6e0dec67352cee6722c6ee2b26f9a2
lplasencia <lplasenciaamaya@gmail.com>
2022-10-15 18:55:25 -0500

    Mensaje de error de credenciales login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,55 +13,45 @@
   <link rel="stylesheet" href="../../plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 </head>
+
 
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="{{route('welcome')}}"><b>INEG</b>Vol.02</a>
+    <a style="text-decoration:none;" href="{{route('welcome')}}"><b>INEG</b>Vol.02</a>
   </div>
   <!-- /.login-logo -->
   <div class="card">
     <div class="card-body login-card-body">
       <p class="login-box-msg">Sign in para iniciar tu sesión</p>
-
       <form method="POST" action="{{ route('login') }}">
         @csrf
+        @error('email')
+        <div class="alert alert-danger" role="alert">
+            <strong>{{ "Email o Password invalidos" }}</strong>
+        </div>  
+        @enderror
         <div class="input-group mb-3">
-          <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" 
+          <input type="email" name="email" class="form-control" 
           value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email">
           <div class="input-group-append">
             <div class="input-group-text">
-              @error('email')
-                <span class="invalid-feedback" role="alert">
-                </span>
-              @enderror
               <span class="fas fa-envelope"></span>
             </div>
           </div>
-          @error('email')
-            <span class="invalid-feedback" role="alert">
-              <strong>{{ "Email o Password invalidos" }}</strong>
-            </span>
-          @enderror
         </div>
         <div class="input-group mb-3">
           <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" 
           required autocomplete="current-password" placeholder="Password">
           <div class="input-group-append">
             <div class="input-group-text">
-              @error('password')
-                <span class="invalid-feedback" role="alert">
-                </span>
-              @enderror
               <span class="fas fa-lock"></span>
             </div>
           </div>
-          @error('password')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-          @enderror
         </div>
         <div class="row">
           <div class="col-8">
